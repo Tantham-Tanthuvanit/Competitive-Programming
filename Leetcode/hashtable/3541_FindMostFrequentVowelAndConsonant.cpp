908c4a97f967090f12bcd7a4db8403034e48ce77
Tantham Tanthuvanit <tantham.tanthuvanit.tan@gmail.com>
2026-01-31 17:16:03 +0700

    Stuff

--- Leetcode/hashtable/3541_FindMostFrequentVowelAndConsonant.cpp
+++ Leetcode/hashtable/3541_FindMostFrequentVowelAndConsonant.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-	int maxFreqSum(string s) {
-		for 
+	bool is_vowel(char c) {
+		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
 	}
-}
+	int maxFreqSum(string s) {
+
+		int vowel = 0;
+		int consonant = 0;
+		
+		unordered_map<char,int> mp;
+
+		for (char c : s) {
+			mp[c]++;
+		}
+
+		for (char ch = 'a'; ch <= 'z'; ch++) {
+			if (is_vowel(ch)) {
+				vowel = max(vowel, mp[ch]);
+			} else {
+				consonant = max(consonant, mp[ch]);
+			}
+		}
+		
+		return vowel + consonant;
+
+	}
+};
 
 int main() {
 	return 0;
