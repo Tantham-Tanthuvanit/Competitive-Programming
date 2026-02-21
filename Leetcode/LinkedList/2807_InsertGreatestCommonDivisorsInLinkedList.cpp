1d37baf2539784fbd568e1b1466f1ca024b02ad7
Tantham Tanthuvanit <tantham.tanthuvanit.tan@gmail.com>
2026-02-21 13:49:02 +0000

    dsadsad

---
+++ Leetcode/LinkedList/2807_InsertGreatestCommonDivisorsInLinkedList.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <numeric>
+#include <type_traits>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode () : val(0), next(nullptr) {}
+	ListNode (int x) : val(x), next(nullptr) {}
+	ListNode (int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	ListNode* insertGreatestCommonDivisors(ListNode* head) {
+		ListNode* node1 = head;
+		ListNode* node2 = head->next;
+
+		while (node2 != nullptr) {
+			ListNode* newNode = new ListNode(gcd(node1->val, node2->val));
+
+			node1->next = newNode;
+			newNode->next = node2;
+
+			node1 = node2;
+			node2 = node2->next;
+
+		}
+
+		return head;
+
+	}
+};
